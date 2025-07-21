a76ac70420631371af25a7dec2d582e43d426867
Peter Turczak <peter@turczak.de>
2025-07-21 20:26:09 +0200

    Adapt to new backend

--- website/js/ampel.js
+++ website/js/ampel.js
@@ -1,115 +1,80 @@
-traffic_light = function (subdir, root_element) {
-	let me = this;
-	this.error_count = 0;
-	this.subdir = subdir;
-	this.root_element = root_element;
-	this.group_key = "";
-	this.run=function(interval){
-		$.ajax(me.subdir, {
-			dataType: "json",
-			timeout: interval,
-			success: function(data){
-					me.got_answer(data);
-				   },
-			error: function(jqXHR, textStatus, errorThrown){
-				me.error(textStatus);	
-			}
-		    });
-		window.setTimeout(function(){me.run(interval);}, interval );
-		};
-	this.set_way=function(value){
-			$.post(me.subdir,
-				{
-					"giveway": value,
-					"key": me.group_key
-				},
-			);
-		};
-	this.error=function(textStatus){
-		me.error_count++;
-		if (me.error_count>4){
-			me.red_circle.hide();
-			me.yellow_circle.hide();
-			me.green_circle.hide();
-		}
-	};
-	this.got_answer=function(data){
-		me.error_count = 0;
-		$(".battvoltage", me.root_element).val(data.batt_voltage/100.0);
-		if (data.lamp_currents[0]>10) me.red_circle.show(); else me.red_circle.hide();
-		if (data.lamp_currents[1]>10) me.yellow_circle.show(); else me.yellow_circle.hide();
-		if (data.lamp_currents[2]>10) me.green_circle.show(); else me.green_circle.hide();
+traffic_light = function (name, root_element) {
+        let me = this;
+        this.error_count = 0;
+        this.name = name;
+        this.root_element = root_element;
+        this.group_key = "";
+        this.loaded = false;
+        this.set_way=function(value){
+                        $.post(me.name,
+                                {
+                                        "giveway": value,
+                                        "key": me.group_key
+                                },
+                        );
+                };
+        this.error=function(textStatus){
+                me.error_count++;
+                if (me.error_count>4){
+                        me.red_circle.hide();
+                        me.yellow_circle.hide();
+                        me.green_circle.hide();
+                }
+        };
+        this.update=function(data){
+                if (! me.loaded) return;
+                me.error_count = 0;
+                $(".battvoltage", me.root_element).val(data.batt_voltage/100.0);
+                if (data.lamp_currents[0]>10) me.red_circle.show(); else me.red_circle.hide();
+                if (data.lamp_currents[1]>10) me.yellow_circle.show(); else me.yellow_circle.hide();
+                if (data.lamp_currents[2]>10) me.green_circle.show(); else me.green_circle.hide();
 
-	};
-	$.ajax("/image/ampel.svg",
-		{
-		dataType:"text",
-		success:function(data){
-			$(".tlpicture", me.root_element).replaceWith(data);
-			me.red_circle = $(".red", me.root_element);
-			me.yellow_circle = $(".yellow", me.root_element);
-			me.green_circle = $(".green", me.root_element);
-			me.error_count = 0;
-            // Now finally run the stuff after loading
-            me.run(250);
-			},
-		
-		});
-	$(".setred",this.root_element).click( function(){
-		me.set_way(0);
-	});
-	$(".setgreen",this.root_element).click( function(){
-		me.set_way(1);
-	});
-	let groupkey = $(".groupkey",this.root_element);
-	groupkey.change(function(data){
-		me.group_key = groupkey.val();
-	});
+        };
+        $.ajax("/image/ampel.svg",
+                {
+                dataType:"text",
+                success:function(data){
+                        $(".tlpicture", me.root_element).replaceWith(data);
+                        me.red_circle = $(".red", me.root_element);
+                        me.yellow_circle = $(".yellow", me.root_element);
+                        me.green_circle = $(".green", me.root_element);
+                        me.error_count = 0;
+                        me.loaded = true;
+                        me.root_element.show();
+                }
+        });
+        $(".setred",this.root_element).click( function(){
+                me.set_way(0);
+        });
+        $(".setgreen",this.root_element).click( function(){
+                me.set_way(1);
+        });
 }
 
-traffic_lights=[];
-$("document").ready(function()
-{
-	$.ajax("/interface",
-		{
-		dataType:"json",
-		success:function(data){
-            let original = $(".traffic_light_area");
-            let skel = original.clone();
-            original.detach();
-            let section = $("#uebersicht");//skel.parent();
-            for (var x in data){
-                let name = data[x];
-                let local_copy = skel.clone();
-                local_copy.attr("id", name);
-                new traffic_light("/interface/"+name, local_copy);
-                section.append(local_copy);
-            }
-		},
-	});
+let traffic_lights={};
+
+$("document").ready(function() {
+        setInterval( () => {
+                $.ajax("/api/v1/states",
+                        {
+                        dataType:"json",
+                        success:function(data){
+                            let original = $(".traffic_light_area.template");
+                            let skel = original.clone();
+                            skel.removeClass("template");
+                            original.hide();
+                            let section = $("#uebersicht");
+                            for (var name in data){
+                                if (!(name in traffic_lights)) {
+                                        let local_copy = skel.clone();
+                                        local_copy.attr("id", name);
+                                        traffic_lights[name] = new traffic_light(name, local_copy);
+                                        section.append(local_copy);
+                                }
+                                traffic_lights[name].update(data[name]);
+                            }
+                        },
+                });
+        }, 1000);
 });
 
-function hex(x) { return (x.map(function (x) {return x.toString(16);})).join("") }
-
-transportWrapper={
-		key:'',
-		encapsulate: function(challenge, message){
-			message['_time'] = new Date();
-			message['challenge'] = challenge;
-			let hmac = new sha256.HMAC(transportWrapper.key);
-			let raw = JSON.stringify(message);
-			hmac.update(raw);
-			let result = { hash:hex(hmac.digest()),
-						   raw:raw};
-			return result;
-		},
-		decapsulate: function(message){
-			let hmac = new sha256.HMAC(transportWrapper.key);
-			hmac.update(message.raw);
-			if ( hex(hmac.digest()) === message.hash ){
-					return JSON.parse(message.raw);
-			} else {
-					return null;
-			}
-		},
-};
