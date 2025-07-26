1615ff057bd56f5ca62afa9f5246b968bd66c081
Peter Turczak <peter@turczak.de>
2025-07-26 19:17:57 +0200

    Add webserver

---
+++ python/webserver.py
@@ -0,0 +1,66 @@
+import json
+import logging
+import re
+from flask import Flask, request, jsonify
+from os import environ
+import paho.mqtt.client as mqtt
+
+
+logger = logging.getLogger("MQTT")
+
+states = {}
+mqtt_conn = None
+
+
+def handle_state(client, user_data, message):
+    m = re.search(r"ampel/(.[a-zA-Z0-9/]+)/state", message.topic)
+    if m:
+        name = m.group(0)
+        try:
+            states[name] = json.loads(message.payload)
+        except json.JSONDeocdeError:
+            logger.error("Could not decode %s for %s", message.payload, name)
+
+
+def on_connected():
+    logger.debug("Adding subscritions")
+    mqtt_conn.subscribe("ampel/+/state")
+    mqtt_conn.message_callback_add("ampel/+/state", handle_state)
+
+
+def connect_mqtt():
+    mqtt_conn = mqtt.Client()
+    mqtt_conn.username_pw_set(
+        username=environ["MQTT_USER"], password=environ["MQTT_PASS"]
+    )
+
+    mqtt_conn.on_connect = on_connected
+    mqtt_conn.connect(environ["MQTT_HOST"], int(environ["MQTT_PORT"]))
+    return mqtt_conn
+
+
+mqtt_conn = connect_mqtt()
+app = Flask(__name__)
+
+
+@app.route("/")
+def slash():
+    return app.send_static_file("static/index.html")
+
+
+@app.route("/api/v1/states", methods=["GET"])
+def get_state():
+    return jsonify(states), 200
+
+
+@app.route("/api/v1/command", methods=["POST"])
+def send_command():
+    if not request.json or request.json not in [True, False]:
+        return jsonify({"message": "bad request"}), 400
+
+    give_way = request.json
+
+    payload = json.dumps({"give_way": bool(give_way)})
+    mqtt_conn.publish("ampel/command", payload, retain=True)
+    logger.debug("Published command!")
+    return jsonify({"message": "ok"}), 200
